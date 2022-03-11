682e20c33aae81a8a74e4fa0dba31c8d92c59f5a
Tim Soethout <git@timsoethout.nl>
2022-03-11 17:01:50 +0100

    Add example with withdraw retVal

--- src/test/scala/CbcDeciderTest.scala
+++ src/test/scala/CbcDeciderTest.scala
@@ -29,5 +29,30 @@ class CbcDeciderTest extends AnyFlatSpec with should.Matchers {
     decider.allow(100, Seq(Withdraw(10),Deposit(10),Withdraw(10)), Withdraw(10)) shouldBe Accept
   }
 
-  // TODO add example with balance return values to show where modeling influences condition
+  //balance in return values to show where modeling influences condition
+  "Example with balance in return values" should "not allow parallel processing" in {
+    type Money = BigDecimal
+    object AccountRetValEntity extends Entity {
+      override type Operation = AccountOperation
+      override type State = Money
+
+      sealed trait AccountOperation extends OperationType
+      case class Deposit(amount: Money) extends AccountOperation {
+        override type ReturnValue = Money
+
+        override def returnValue(state: Money): Money = state + amount
+
+        override def effect(state: Money): Money = state + amount
+      }
+      case class Withdraw(amount: Money) extends AccountOperation {
+        override type ReturnValue = Money
+
+        override def returnValue(state: Money): Money = if state - amount >= 0 then state - amount else state
+
+        override def effect(state: Money): Money = if state - amount >= 0 then state - amount else state
+      }
+    }
+    val balRetValDecider: CbcDecider[AccountRetValEntity.type] = new CbcDecider(AccountRetValEntity)
+    balRetValDecider.allow(100, Seq(AccountRetValEntity.Deposit(10)), AccountRetValEntity.Deposit(100)) shouldBe Delay
+  }
 }
