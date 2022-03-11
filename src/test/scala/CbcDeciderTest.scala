4dd2ba887ce9c72eb4e517d886ed70d4d55e5e4f
Tim Soethout <git@timsoethout.nl>
2022-03-11 17:09:29 +0100

    modular tests

--- src/test/scala/CbcDeciderTest.scala
+++ src/test/scala/CbcDeciderTest.scala
@@ -3,12 +3,13 @@ package com.ing.loca
 import LoCADecider.{Accept, Delay, Reject}
 import example.AccountEntity
 
-import com.ing.loca.example.AccountEntity.{Deposit, Withdraw}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class CbcDeciderTest extends AnyFlatSpec with should.Matchers {
   behavior of "CbcDecider"
+
+  import example.AccountEntity.{Deposit, Withdraw}
 
   val decider: CbcDecider[AccountEntity.type] = new CbcDecider(AccountEntity)
 
@@ -25,34 +26,43 @@ class CbcDeciderTest extends AnyFlatSpec with should.Matchers {
   }
 
   it should "allow parallel withdrawals sufficient balance is available with multiple in progress" in {
-    decider.allow(100, Seq(Withdraw(10),Withdraw(10),Withdraw(10)), Withdraw(10)) shouldBe Accept
-    decider.allow(100, Seq(Withdraw(10),Deposit(10),Withdraw(10)), Withdraw(10)) shouldBe Accept
+    decider.allow(100, Seq(Withdraw(10), Withdraw(10), Withdraw(10)), Withdraw(10)) shouldBe Accept
+    decider.allow(100, Seq(Withdraw(10), Deposit(10), Withdraw(10)), Withdraw(10)) shouldBe Accept
   }
+}
+
+class CbcDeciderRetValTest extends AnyFlatSpec with should.Matchers {
+  type Money = BigDecimal
+  object AccountRetValEntity extends Entity {
+    override type Operation = AccountOperation
+    override type State = Money
+
+    sealed trait AccountOperation extends OperationType
+    case class Deposit(amount: Money) extends AccountOperation {
+      override type ReturnValue = Money
+
+      override def returnValue(state: Money): Money = state + amount
+
+      override def effect(state: Money): Money = state + amount
+    }
+    case class Withdraw(amount: Money) extends AccountOperation {
+      override type ReturnValue = Money
+
+      override def returnValue(state: Money): Money = if state - amount >= 0 then state - amount else state
+
+      override def effect(state: Money): Money = if state - amount >= 0 then state - amount else state
+    }
+  }
+
+  import AccountRetValEntity.*
+
+  val balRetValDecider: CbcDecider[AccountRetValEntity.type] = new CbcDecider(AccountRetValEntity)
 
   //balance in return values to show where modeling influences condition
   "Example with balance in return values" should "not allow parallel processing" in {
-    type Money = BigDecimal
-    object AccountRetValEntity extends Entity {
-      override type Operation = AccountOperation
-      override type State = Money
-
-      sealed trait AccountOperation extends OperationType
-      case class Deposit(amount: Money) extends AccountOperation {
-        override type ReturnValue = Money
-
-        override def returnValue(state: Money): Money = state + amount
-
-        override def effect(state: Money): Money = state + amount
-      }
-      case class Withdraw(amount: Money) extends AccountOperation {
-        override type ReturnValue = Money
-
-        override def returnValue(state: Money): Money = if state - amount >= 0 then state - amount else state
-
-        override def effect(state: Money): Money = if state - amount >= 0 then state - amount else state
-      }
-    }
-    val balRetValDecider: CbcDecider[AccountRetValEntity.type] = new CbcDecider(AccountRetValEntity)
-    balRetValDecider.allow(100, Seq(AccountRetValEntity.Deposit(10)), AccountRetValEntity.Deposit(100)) shouldBe Delay
+    balRetValDecider.allow(100, Seq(Deposit(10)), Deposit(100)) shouldBe Delay
+    balRetValDecider.allow(100, Seq(Withdraw(10)), Withdraw(100)) shouldBe Delay
+    balRetValDecider.allow(100, Seq(Withdraw(10), Withdraw(10), Withdraw(10)), Withdraw(10)) shouldBe Delay
+    balRetValDecider.allow(100, Seq(Withdraw(10), Deposit(10), Withdraw(10)), Withdraw(10)) shouldBe Delay
   }
 }
